cefc7696589607a236e63a532c52c8871a78bb3f
teenajangid <teenajangid2020@gmail.com>
2026-04-10 17:25:02 +0530

    error solve

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-  PHASE_PRODUCTION_SERVER,
-} from "next/constants";
 
-export default function nextConfig(phase: string): NextConfig {
-  const distDir =
-    phase === PHASE_DEVELOPMENT_SERVER
-      ? ".next/dev"
-      : phase === PHASE_PRODUCTION_BUILD || phase === PHASE_PRODUCTION_SERVER
-        ? ".next/prod"
-        : ".next/shared";
+const nextConfig: NextConfig = {};
 
-  return {
-    distDir,
-  };
-}
+export default nextConfig;
